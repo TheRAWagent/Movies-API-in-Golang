e3a08ebd469e7ee72d8d9e5684939d73dced958b
Dhairya Joshi <77039476+TheRAWagent@users.noreply.github.com>
2024-03-11 18:10:02 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	fmt.Println("Hello, World!")
+	movies = append(movies, Movie{ID: "1", Isbn: "448743", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "448744", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
+
+	// Init Router
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", GetAllMovies).Methods("GET")
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello, World!")
+	}).Methods("GET")
+	r.HandleFunc("/movies/{id}", DeleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
+	r.HandleFunc("/movies", CreateMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", UpdateMovie).Methods("PUT")
+
+	// Start Server
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
